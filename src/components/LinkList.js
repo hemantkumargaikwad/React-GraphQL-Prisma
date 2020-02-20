ad9d3a8d14623b69b5f5ad8452631799a24f2631
Hemant Gaikwad <hemant.gaikwad@synerzip.com>
2020-02-20 11:48:08 +0530

    React application

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Link from './Link';
-import { Query } from 'react-apollo'
-import gql from 'graphql-tag'
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
 const FEED_QUERY = gql`
 	{
 		feed {
@@ -10,30 +10,40 @@ const FEED_QUERY = gql`
 				createdAt
 				url
 				description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
 			}
 		}
 	}
 `;
 
 class LinkList extends Component {
-    render() {
-      return (
-        <Query query={FEED_QUERY}>
-          {({ loading, error, data }) => {
-            if (loading) return <div>Fetching</div>
-            if (error) return <div>Error</div>
-      
-            const linksToRender = data.feed.links
-      
-            return (
-              <div>
-                {linksToRender.map(link => <Link key={link.id} link={link} />)}
-              </div>
-            )
-          }}
-        </Query>
-      )
-    }
-  }
+	render() {
+		return (
+			<Query query={FEED_QUERY}>
+				{({ loading, error, data }) => {
+					if (loading) return <div>Fetching</div>;
+					if (error) return <div>Error</div>;
+
+					const linksToRender = data.feed.links;
+
+					return (
+						<div>
+							{linksToRender.map((link, index) => <Link key={link.id} link={link} index={index} />)}
+						</div>
+					);
+				}}
+			</Query>
+		);
+	}
+}
 
 export default LinkList;
